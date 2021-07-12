32618f07a724e850340a6f53b5f7d5786f579f56
Michael Ngo <michaelngo1337@yahoo.com>
2021-07-12 09:49:02 -0700

    changed app.listen

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App is running!')
+})
